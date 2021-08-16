cfff6ca7715725b6189efb4d5f3cb0959bfe0784
danielemaraschin <danikmaraschin@gmail.com>
2021-08-16 22:23:22 +0200

    Learning about 'test' instead of 'it'

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
- import App from './App'
+import App from './App'
 describe('Component principal', () => {
-    it('Mostrar o nome do banco', () => {
-        render(<App />);
+    describe('Quando eu abro a app do banco,', () => {
 
-        expect(screen.getByText('ByteBank')).toBeInTheDocument();
+        test(' o nome do banco é exibido ', () => {
+            render(<App />);
+
+            expect(screen.getByText('ByteBank')).toBeInTheDocument();
+        })
+
+
+        it(' o saldo é exibido', () => {
+            render(<App />);
+            expect(screen.getByText('Saldo:')).toBeInTheDocument();
+        })
+
+        it('Quando eu abro a app do banco, o botão de transações é exibido', () => {
+            render(<App />);
+            expect(screen.getByText('Realizar operação')).toBeInTheDocument();
+        })
     })
-})
-
-it('Mostrar saldo', () => {
-    render(<App />);
-    expect(screen.getByText('Saldo:')).toBeInTheDocument();
 })
