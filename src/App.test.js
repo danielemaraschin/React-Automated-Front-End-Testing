ffedfd28fc88ee44cfbee40330f6e24d8136b5af
danielemaraschin <danikmaraschin@gmail.com>
2021-08-17 13:45:43 +0200

    Testing a function

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import App, { calcularNovoSaldo} from './App'
 describe('Component principal', () => {
     describe('Quando eu abro a app do banco,', () => {
 
-        test(' o nome do banco é exibido ', () => {
+        it(' o nome do banco é exibido ', () => {
             render(<App />);
 
             expect(screen.getByText('ByteBank')).toBeInTheDocument();
@@ -30,7 +30,16 @@ describe('Component principal', () => {
             }
             const saldoInicial = 150
             const novoSaldo = calcularNovoSaldo(valores, saldoInicial)
-            expect(novoSaldo).toBe(6)
+            expect(novoSaldo).toBe(100)
+        });
+        it('que é um depóstio, o valor vai aumentar', () => {
+            const valores = {
+                transacao: 'deposito',
+                valor: 50
+            }
+            const saldoInicial = 150
+            const novoSaldo = calcularNovoSaldo(valores, saldoInicial)
+            expect(novoSaldo).toBe(200)
         });
     })
 })
