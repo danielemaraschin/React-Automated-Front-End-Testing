93113e97728f211fc599ac032ca96a981aceba4b
danielemaraschin <danikmaraschin@gmail.com>
2021-09-10 10:28:18 +0200

    working with: monetary value

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,3 +1,13 @@
+import React from 'react' //because we are testing a react component
+import { render, screen} from "@testing-library/react";
+import Conta from './Conta'
+
+
 describe('Componente Conta', () =>{
-    it('Exibir o saldo da conta com formatação monetária')
+    it('Exibir o saldo da conta como valor monetário', () =>{
+    render(<Conta saldo={1000}/>);
+    const saldo = screen.getByTestId('saldo-conta')
+  
+    expect(saldo.textContent).toBe('R$ 1000');    
+    })    
 })
