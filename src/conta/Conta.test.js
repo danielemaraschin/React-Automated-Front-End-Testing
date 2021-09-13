5825b0f956ac9255b98ac1ad48f4b61a38ed39b4
danielemaraschin <danikmaraschin@gmail.com>
2021-09-13 13:16:55 +0200

    working with fireEvent.click

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -2,7 +2,6 @@ import React from 'react' //because we are testing a react component
 import { render, screen, fireEvent} from "@testing-library/react";
 import Conta from './Conta'
 
-
 describe('Componente Conta', () =>{
     it('Exibir o saldo da conta como valor monetário', () =>{
     render(<Conta saldo={1000}/>);
@@ -12,11 +11,18 @@ describe('Componente Conta', () =>{
     }) 
     
     it('Chama a função realizar transação quando o botão é clicado', () => {
-        const funcaoRealizarTransacao = jest.fn() //essa funcao nao faz nd, é só p ver se ela eh chamada ao clicar no botao
-    //se essa funcao for chamada, é pq o botao esta chamando a funcao que eh 2do parametro do componente 'conta'
-    render(<Conta saldo={1000} realizarTransacao= {funcaoRealizarTransacao} />) //nao é relevante para esse teste o saldo, entao podemos passar um valor qualquer aqui
+        const funcaoRealizarTransacao = jest.fn() //essa funcao nao faz nd,
+    //jest.fn é chamada de  'spy', é só p ver se ela eh chamada ao clicar
+    //no botao se essa funcao for chamada, é pq o botao esta chamando
+    //a funcao que eh 2do parametro do componente 'conta'
+    render(<Conta saldo={1000} realizarTransacao= {funcaoRealizarTransacao} />) 
+    //nao é relevante para esse teste o saldo, entao podemos passar um valor qualquer
     
-        
+    fireEvent.click(screen.getByText('Realizar operação'));
+    //teoricamente o teste nao deveria ter passado, ja q nao cliquei no botao antes de 
+    //realizar o teste, mas no da prof nao passou entao add fireEvent.click,
+    //no meu seguiu passando, da prof passou agora so c/o 
+    // o fireEvent 'clica' no nosso botao 1x    
     expect(funcaoRealizarTransacao).toHaveBeenCalled
     })
 })
